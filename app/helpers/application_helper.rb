4da40e40c511b407e328ae80847e8c77ec810f9e
erikhicks <erik.hicks@stripes39.com>
2012-03-07 12:11:38 -0800

    Page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def main_title
+    'Horticulture With Ms. H. at Monroe High School, Monroe, WA'
+  end
+
   def nav_link(name, url)
     content_tag :li, :class => nav_link_class(url) do 
       link_to "#{name}", "/#{url}/"
