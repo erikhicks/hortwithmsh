a492e05fe2579d9d20af488ff51bcae7ac552e94
Erik Hicks <ehicks@gmail.com>
2012-09-17 19:47:38 -0700

    Fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ namespace :deploy do
     run "cd #{release_path}/ && bundle exec rake assets:precompile"  
   end
   task :material_symlink do
-    run "ln -nfs #{shared_path}/materials #{release_path}/public/materials"
+    run "ln -nfs #{release_path}/public/materials #{shared_path}/materials"
   end
 end
 
